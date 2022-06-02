d5eb84f29d633fa499faf400d31e87db04700882
Jakub Štěpánek <jakub.stepanek@tul.cz>
2022-06-02 17:06:33 +0200

    Backup

--- RaceResults/src/ui/App.java
+++ RaceResults/src/ui/App.java
@@ -2,23 +2,20 @@ package ui;
 //use j2html
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-import app.Circuit;
 import app.ComparatorRacerBySurname;
 import app.Nationality;
 import app.Race;
 import app.Racer;
 import utils.FileExplorer;
+import utils.RaceUtils;
 
 public class App {
     private static Scanner sc = new Scanner(System.in);
@@ -56,7 +53,7 @@ public class App {
                         }
                         break;
                     case "2":
-                        Race raceByUser = createNewRace();
+                        Race raceByUser = RaceUtils.createNewRace();
                         boolean exitNewRaceMenu = false;
                         while (!exitNewRaceMenu) {
                             showNewRaceMenu();
@@ -64,7 +61,7 @@ public class App {
                                 case "1":
                                     // add racer
                                     try {
-                                        raceByUser.addRacer(addRacerByUser());
+                                        raceByUser.addRacer(RaceUtils.addRacerByUser());
                                     } catch (IllegalArgumentException e) {
                                         System.out.println(e.getMessage());
                                     }
@@ -77,7 +74,7 @@ public class App {
                                     // delete racer
                                     System.out.print("Zadejte příjmení závodníka: ");
                                     String surname = sc.nextLine();
-                                    raceByUser.deleteRacer(findRacer(raceByUser, surname));
+                                    raceByUser.deleteRacer(RaceUtils.findRacer(raceByUser, surname));
                                     System.out.println("Závodník smazán.");
                                     break;
                                 case "q":
@@ -91,28 +88,28 @@ public class App {
                         System.out.print("Přejete si závod uložit? [ano/ne]: ");
                         String save = sc.nextLine();
                         if (save.equalsIgnoreCase("ano")) {
-                            saveRace(raceByUser, raceByUser.getCircuitName() + raceByUser.getSeasonYear());
+                            RaceUtils.saveRace(raceByUser, raceByUser.getCircuitName() + raceByUser.getSeasonYear());
                         }
                         break;
                     case "3":
                         // TODO: change values of RACER
-                        if (areRacersLoaded(race)) {
+                        if (RaceUtils.areRacersLoaded(race)) {
                             System.out.println("Nejdřív musíte načíst závod!");
                             break;
                         }
                         System.out.print("Zadej příjmení závodníka: ");
                         String surname = sc.nextLine();
-                        findRacers(race, surname);
+                        RaceUtils.findRacers(race, surname);
                         break;
                     case "4":
                         showRacerInterDatabase();
                         break;
                     case "5":
-                        if (areRacersLoaded(race)) {
+                        if (RaceUtils.areRacersLoaded(race)) {
                             System.out.println("Nejdřív musíte načíst závod!");
                             break;
                         } else {
-                            saveRace(race, race.getCircuitName() + race.getSeasonYear());
+                            RaceUtils.saveRace(race, race.getCircuitName() + race.getSeasonYear());
                         }
                         break;
                     case "q":
@@ -142,42 +139,11 @@ public class App {
         System.out.print("Vyberte jednu z možností: ");
     }
 
-    private void saveRace(Race race, String fileName) throws IOException {
-        String result = System.getProperty("user.dir") + File.separator + "Data" + File.separator
-                + fileName + ".csv";
-        // new PrintWriter(new OutputStreamWriter () pouzit, kdyz chci kodovani
-        try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(result, true)))) {
-            pw.println(String.format("%-8s %-10s %-10s %-15s %-30s %-10s %-15s %-2s %s", "Umístění",
-                    "Jméno",
-                    "Příjmení", "Národnost", "Tým", "Motocykl", "Startovní číslo",
-                    "Max. rychlost", "Čas"));
-            for (Racer racer : race.getRacers()) {
-                pw.println(racer);
-            }
-        }
-
-    }
-
-    public void addPointsToInternalDatabase(Race race) {
-        ArrayList<Racer> loadedRacers = race.getRacers();
-        for (Racer racer : racerStats) {
-            for (int i = 0; i < loadedRacers.size(); i++) {
-                if (racer.getRacingNumber().equals(loadedRacers.get(i).getRacingNumber())) {
-                    racer.setPoints(racer.getPoints() + loadedRacers.get(i).getPoints());
-                }
-            }
-        }
-    }
-
-    public static boolean areRacersLoaded(Race race) {
-        return race.getRacers().size() == 0;
-    }
-
-    private void initialization() throws FileNotFoundException, IOException {
+    private ArrayList<Racer> initialization() throws FileNotFoundException, IOException {
         String line;
         String[] parts;
         Racer r;
-        String parentPath = System.getProperty("user.dir") + File.separator + "Data" + File.separator
+        String parentPath = System.getProperty("user.dir") + File.separator + "data" + File.separator
                 + "riders.csv";
         File dataDirectory = new File(parentPath);
         try (BufferedReader br = new BufferedReader(new FileReader(dataDirectory))) {
@@ -195,39 +161,8 @@ public class App {
                 racerStats.add(r);
             }
         }
+        return racerStats;
 
-    }
-
-    public static Race createNewRace() {
-        // TODO: add new racer to riders.csv
-        Race raceByUser = new Race();
-        System.out.print("Zadejte sezónu, kdy se jel závod: ");
-        raceByUser.setSeasonYear(sc.nextInt());
-        System.out.print("Zadejte název okruhu: ");
-        sc.nextLine();
-        raceByUser.setCircuitName(Circuit.of(sc.nextLine()));
-        String answer;
-        do {// TODO: add racers to raceByUser InternalDB
-            System.out.print("Přejete si přidat závodníka? [ano/ne]: ");
-            answer = sc.nextLine().toLowerCase();
-            if (answer.equalsIgnoreCase("ano")) {
-                raceByUser.addRacer(addRacerByUser());
-            }
-        } while (answer.equalsIgnoreCase("ano"));
-
-        return raceByUser;
-    }
-
-    public static Racer addRacerByUser() {
-        System.out.print("Zadejte jméno závodníka: ");
-        String name = sc.nextLine();
-        System.out.print("Zadejte příjmení závodníka: ");
-        String surname = sc.nextLine();
-        System.out.print("Zadejte zkratku národnosti: ");
-        Nationality nationality = Nationality.valueOf(sc.nextLine().toUpperCase());
-        Racer r = new Racer(name, surname, nationality);
-        System.out.println(r);
-        return r;
     }
 
     public static void loadResults(Race race) throws FileNotFoundException {
@@ -238,7 +173,7 @@ public class App {
                             + userAnswer);
             race.loadStats(file);
             System.out.println(race);
-            if (showRacerDetail(race)) {
+            if (RaceUtils.showRacerDetail(race)) {
                 System.out.print("Zadej příjmení jezdce: ");
                 String surname = sc.nextLine();
                 System.out.println(
@@ -246,42 +181,12 @@ public class App {
                                 "Jméno",
                                 "Příjmení", "Národnost", "Tým", "Motocykl", "Startovní číslo",
                                 "Max. rychlost", "Čas"));
-                findRacers(race, surname);
+                RaceUtils.findRacers(race, surname);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-    }
-
-    public static ArrayList<Racer> findRacers(Race race, String surname) {
-        ArrayList<Racer> foundRacers = race.getRacer(surname);
-        if (foundRacers.size() == 0) {
-            throw new IllegalArgumentException("Závodníka se zadaným příjmením nelze najít!");
-        } else {
-            // for (Racer racer : foundRacers) {
-            // System.out.println(racer);
-            // }
-            return foundRacers;
-        }
-    }
-
-    public static Racer findRacer(Race race, String surname) {
-        ArrayList<Racer> foundRacers = race.getRacer(surname);
-        if (foundRacers.size() == 0) {
-            throw new IllegalArgumentException("Závodníka se zadaným příjmením nelze najít!");
-        } else {
-            // for (Racer racer : foundRacers) {
-            // System.out.println(racer);
-            // }
-            return foundRacers.get(0);
-        }
-    }
-
-    public static boolean showRacerDetail(Race race) {
-        System.out.println("Chceš zobrazit detail jezdce? [a/n]");
-        String answer = sc.nextLine().toLowerCase();
-        return answer.equals("a");
     }
 
     public static void showMainMenu() {
---
+++ RaceResults/src/utils/RaceUtils.java
@@ -0,0 +1,109 @@
+package utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import app.Circuit;
+import app.Nationality;
+import app.Race;
+import app.Racer;
+
+public class RaceUtils {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void saveRace(Race race, String fileName) throws IOException {
+        String result = System.getProperty("user.dir") + File.separator + "Data" + File.separator
+                + fileName + ".csv";
+        // new PrintWriter(new OutputStreamWriter () pouzit, kdyz chci kodovani
+        try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(result, true)))) {
+            pw.println(String.format("%-8s %-10s %-10s %-15s %-30s %-10s %-15s %-2s %s", "Umístění",
+                    "Jméno",
+                    "Příjmení", "Národnost", "Tým", "Motocykl", "Startovní číslo",
+                    "Max. rychlost", "Čas"));
+            for (Racer racer : race.getRacers()) {
+                pw.println(racer);
+            }
+        }
+    }
+
+    // public void addPointsToInternalDatabase(Race race) {
+    // ArrayList<Racer> loadedRacers = race.getRacers();
+    // for (Racer racer : racerStats) {
+    // for (int i = 0; i < loadedRacers.size(); i++) {
+    // if (racer.getRacingNumber().equals(loadedRacers.get(i).getRacingNumber())) {
+    // racer.setPoints(racer.getPoints() + loadedRacers.get(i).getPoints());
+    // }
+    // }
+    // }
+    // }
+    public static boolean areRacersLoaded(Race race) {
+        return race.getRacers().size() == 0;
+    }
+
+    public static Race createNewRace() {
+        // TODO: add new racer to riders.csv
+        Race raceByUser = new Race();
+        System.out.print("Zadejte sezónu, kdy se jel závod: ");
+        raceByUser.setSeasonYear(sc.nextInt());
+        System.out.print("Zadejte název okruhu: ");
+        sc.nextLine();
+        raceByUser.setCircuitName(Circuit.of(sc.nextLine()));
+        String answer;
+        do {// TODO: add racers to raceByUser InternalDB
+            System.out.print("Přejete si přidat závodníka? [ano/ne]: ");
+            answer = sc.nextLine().toLowerCase();
+            if (answer.equalsIgnoreCase("ano")) {
+                raceByUser.addRacer(addRacerByUser());
+            }
+        } while (answer.equalsIgnoreCase("ano"));
+
+        return raceByUser;
+    }
+
+    public static Racer addRacerByUser() {
+        System.out.print("Zadejte jméno závodníka: ");
+        String name = sc.nextLine();
+        System.out.print("Zadejte příjmení závodníka: ");
+        String surname = sc.nextLine();
+        System.out.print("Zadejte zkratku národnosti: ");
+        Nationality nationality = Nationality.valueOf(sc.nextLine().toUpperCase());
+        Racer r = new Racer(name, surname, nationality);
+        System.out.println(r);
+        return r;
+    }
+
+    public static ArrayList<Racer> findRacers(Race race, String surname) {
+        ArrayList<Racer> foundRacers = race.getRacer(surname);
+        if (foundRacers.size() == 0) {
+            throw new IllegalArgumentException("Závodníka se zadaným příjmením nelze najít!");
+        } else {
+            // for (Racer racer : foundRacers) {
+            // System.out.println(racer);
+            // }
+            return foundRacers;
+        }
+    }
+
+    public static Racer findRacer(Race race, String surname) {
+        ArrayList<Racer> foundRacers = race.getRacer(surname);
+        if (foundRacers.size() == 0) {
+            throw new IllegalArgumentException("Závodníka se zadaným příjmením nelze najít!");
+        } else {
+            // for (Racer racer : foundRacers) {
+            // System.out.println(racer);
+            // }
+            return foundRacers.get(0);
+        }
+    }
+
+    public static boolean showRacerDetail(Race race) {
+        System.out.println("Chceš zobrazit detail jezdce? [a/n]");
+        String answer = sc.nextLine().toLowerCase();
+        return answer.equals("a");
+    }
+}
